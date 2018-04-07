8a57b92c63ff9d975675186fda41d60a064197bf
wenhao <wenhao@freeloops.local>
2018-04-07 22:33:24 +0800

    begin deployment to heroku

--- index.js
+++ index.js
@@ -94,7 +94,8 @@ app.get('*', (request, response) => {
  * Listen to requests on port 3000
  * ===================================
  */
-const server = app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, () => console.log('~~~ Tuning in to the waves of port '+PORT+' ~~~'));
 
 server.on('close', () => {
   console.log('Closed express server');
